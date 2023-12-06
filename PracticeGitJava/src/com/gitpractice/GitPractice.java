454e3d83f54e64ac6ea8b217f34e771d88d3a628
Ramji <100006991+javarangers@users.noreply.github.com>
2023-12-05 20:00:42 -0500

    changing in my Java code

--- PracticeGitJava/src/com/gitpractice/GitPractice.java
+++ PracticeGitJava/src/com/gitpractice/GitPractice.java
@@ -4,6 +4,7 @@ public class GitPractice {
 
 	public static void main(String[] args) {
 		System.out.println("this is first git project I am learning");
+		System.out.println("Hello there how are you?");
 
 	}
 
