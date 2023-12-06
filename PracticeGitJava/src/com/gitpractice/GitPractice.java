b820050db32b87b9f71d529f68532e034836b57f
Ramji <100006991+javarangers@users.noreply.github.com>
2023-12-05 20:59:41 -0500

    adding more code in java

--- PracticeGitJava/src/com/gitpractice/GitPractice.java
+++ PracticeGitJava/src/com/gitpractice/GitPractice.java
@@ -5,6 +5,9 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("this is first git project I am learning");
 		System.out.println("Hello there how are you?");
+		System.out.println("this is first git project I am learning");
+		System.out.println("Hello there how are you?");
+		
 
 	}
 
